00865383081ccadf83cafd8da42ee5f1f5e7ea87
reg77777 <junpei1216@outlook.jp>
2020-01-28 17:48:37 +0900

    [Update] コードの体裁を整えた

--- bootloader.asm
+++ bootloader.asm
@@ -4,7 +4,7 @@ org 0x7c00
 jmp entry
 
 db 0x90
-db "HELLOIPL"
+db "BOOTLOAD"
 dw 512
 db 1
 dw 1
@@ -16,11 +16,11 @@ dw 9
 dw 18
 dw 2
 dd 0
-DD 2880 
-DB 0,0,0x29
-DD 0xffffffff     
-DB "HELLO-OS   "  
-DB "FAT12   "     
+dd 2880 
+db 0,0,0x29
+dd 0xffffffff     
+db "MYSUGOIOS  "
+db "FAT12   "     
 times 18 db 0
 
 entry:
@@ -28,11 +28,9 @@ mov ax,0
 mov ss,ax
 mov sp,0x7c00
 mov ds,ax
-
 mov ax,0x0820
 mov es,ax
 mov cl,2
-
 mov bx,0
 mov dl,0x00
 
@@ -60,34 +58,13 @@ loop1:
   cmp ch,c
   jb loop1
 
-  jmp 0xc200
-
-error:
-  mov ax,0
-  mov ss,ax
-  mov sp,0x7c00
-  mov ds,ax
-  mov es,ax
-  mov si,msg
-  putloop:
-    MOV AL, [SI]	;0x10 Charactor_code
-    ADD SI, 1			;0x10 Next_word
-    CMP AL, 0
-    JE  fin
-    MOV AH, 0x0e 	;0x10 (Fixation)
-    MOV BX, 15
-    INT 0x10
-    JMP putloop
-
-fin:
-	HLT
-	JMP fin
+jmp 0xc200
 
 msg:
-	DB	0x0a,0x0a
-	DB	"load error"
-	DB	0x0a
-	DB	0
+	db 0x0a,0x0a
+	db "load error"
+	db 0x0a
+	db 0
 
 times 0x7dfe-0x7c00-($-$$) db 0
 db 0x55,0xaa
